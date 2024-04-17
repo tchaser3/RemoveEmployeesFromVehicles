79c157f6ccc2e058ab7aa549a038df773534337f
Terry Holmes <tchaser3@outlook.com>
2024-04-17 07:12:00 -0400

    Add project files.

---
+++ RemoveEmployeesFromVehicles/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿/* Title:           Remove Employees From Vehicles
+ * Date:            5-31-18
+ * Author:          Terry Holmes
+ * 
+ * Description:     This program will change all vehicles with employees that have been let go to the warehouse the vehicle is registered to */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RemoveEmployeesFromVehicles
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
